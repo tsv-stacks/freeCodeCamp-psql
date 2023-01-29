468119adc06334ee3166303819745b7af68222f4
tsv-stacks <tsvirdi@yahoo.co.uk>
2023-01-29 18:48:43 +0000

    finished 95%

--- universe.sql
+++ universe.sql
@@ -51,7 +51,8 @@ CREATE TABLE public.galaxy (
     name character varying(20) NOT NULL,
     type character varying(20) NOT NULL,
     distance_from_earth numeric(5,1),
-    galaxy_id integer NOT NULL
+    galaxy_id integer NOT NULL,
+    diameter integer
 );
 
 
@@ -172,12 +173,12 @@ ALTER TABLE ONLY public.planet ALTER COLUMN planet_id SET DEFAULT nextval('publi
 -- Data for Name: galaxy; Type: TABLE DATA; Schema: public; Owner: freecodecamp
 --
 
-INSERT INTO public.galaxy VALUES ('Milky Way', 'Spiral', 0.0, 1);
-INSERT INTO public.galaxy VALUES ('Messier 87', 'Elliptical', 55.0, 2);
-INSERT INTO public.galaxy VALUES ('Cigar (M82)', 'Irregular', 12.0, 3);
-INSERT INTO public.galaxy VALUES ('Andromeda', 'Spiral', 2.5, 4);
-INSERT INTO public.galaxy VALUES ('Whirlpool (M51)', 'Spiral', 30.0, 5);
-INSERT INTO public.galaxy VALUES ('Triangulum (M33)', 'Unbarred', 3.0, 6);
+INSERT INTO public.galaxy VALUES ('Milky Way', 'Spiral', 0.0, 1, 100000);
+INSERT INTO public.galaxy VALUES ('Andromeda', 'Spiral', 2.5, 4, 220000);
+INSERT INTO public.galaxy VALUES ('Messier 87', 'Elliptical', 55.0, 2, 130000);
+INSERT INTO public.galaxy VALUES ('Cigar (M82)', 'Irregular', 12.0, 3, 14000);
+INSERT INTO public.galaxy VALUES ('Whirlpool (M51)', 'Spiral', 30.0, 5, 60000);
+INSERT INTO public.galaxy VALUES ('Triangulum (M33)', 'Unbarred', 3.0, 6, 60000);
 
 
 --
@@ -198,6 +199,12 @@ INSERT INTO public.moon VALUES ('Oberon', false, 7, 11, 13.5);
 INSERT INTO public.moon VALUES ('Iapetus', true, 6, 12, 79.3);
 INSERT INTO public.moon VALUES ('Hyperion', false, 6, 13, 21.3);
 INSERT INTO public.moon VALUES ('Phobos', false, 4, 14, 0.3);
+INSERT INTO public.moon VALUES ('Ariel', false, 7, 15, 2.5);
+INSERT INTO public.moon VALUES ('Umbriel', false, 7, 16, 4.1);
+INSERT INTO public.moon VALUES ('Proteus', false, 8, 17, 1.2);
+INSERT INTO public.moon VALUES ('Mimas', false, 6, 18, 0.9);
+INSERT INTO public.moon VALUES ('Charon', false, 9, 19, 6.4);
+INSERT INTO public.moon VALUES ('Thebe', false, 5, 20, 0.7);
 
 
 --
@@ -212,6 +219,10 @@ INSERT INTO public.planet VALUES ('Jupiter', 1898.00, 142984, 1326, 23.1, false,
 INSERT INTO public.planet VALUES ('Saturn', 568.00, 120536, 687, 9.0, false, 62, 1433.5, NULL, 1, 1, 6);
 INSERT INTO public.planet VALUES ('Uranus', 86.80, 51118, 1271, 8.7, false, 27, 2872.5, NULL, 1, 1, 7);
 INSERT INTO public.planet VALUES ('Neptune', 102.00, 49528, 1638, 11.0, false, 14, 4495.1, NULL, 1, 1, 8);
+INSERT INTO public.planet VALUES ('Pluto', 0.01, 2377, 203, 0.6, false, 5, 5906.1, NULL, 1, 1, 9);
+INSERT INTO public.planet VALUES ('Ceres', 0.01, 940, 216, 0.3, false, 0, 257.0, NULL, 1, 1, 10);
+INSERT INTO public.planet VALUES ('Eris', 0.01, 2326, 252, 0.8, false, 1, 10125.0, NULL, 1, 1, 11);
+INSERT INTO public.planet VALUES ('Makemake', 0.04, 1430, 203, 0.6, false, 1, 52800.0, NULL, 1, 1, 12);
 
 
 --
@@ -223,13 +234,14 @@ INSERT INTO public.star VALUES ('Siruis', 2, 17934, 1, 2, 'A type main sequence'
 INSERT INTO public.star VALUES ('Betelgeuse', 20, 7012, 1, 3, 'red supergiant');
 INSERT INTO public.star VALUES ('Rigel', 12, 11000, 1, 4, 'blue-white supergiant');
 INSERT INTO public.star VALUES ('Antares', 15, 3500, 1, 5, 'red supergiant');
+INSERT INTO public.star VALUES ('Altair', 1, 7600, 1, 6, 'A type main sequence');
 
 
 --
 -- Name: moon_moon_id_seq; Type: SEQUENCE SET; Schema: public; Owner: freecodecamp
 --
 
-SELECT pg_catalog.setval('public.moon_moon_id_seq', 14, true);
+SELECT pg_catalog.setval('public.moon_moon_id_seq', 20, true);
 
 
 --
